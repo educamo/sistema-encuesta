39782c0f6acfde809130fd8f5b7461167d9c104e
C Eduardo Carrasco <educamo@hotmail.com>
2022-11-23 13:02:50 -0400

    archivo del select2 (revisar si se include)

---
+++ select.php
@@ -0,0 +1,12 @@
+
+<script>
+    $(document).ready(function() {
+
+        $('.mi-selector').select2({
+            placeholder: "Selecciona una persona",
+            allowClear: true
+        });
+
+    });
+</script>
+
